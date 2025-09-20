a5871355ea81da8b2cf6f9d21400272fe91c61c2
Daniil <danyalyapunov@gmail.com>
2025-09-20 13:13:48 +0300

    added error handling and output formatting

--- ProgrammingCorporateSystems(cSharp)/workbook1.2/workbook1.2.1/Program.cs
+++ ProgrammingCorporateSystems(cSharp)/workbook1.2/workbook1.2.1/Program.cs
@@ -3,37 +3,41 @@
 class Program
 {
     static void Main()
-   {
-      Console.Write("Введите номер дня недели, с которого начинается май (1-7): ");
-      if (int.TryParse(Console.ReadLine(), out int startDay) || startDay<1 || startDay > 7)
-      {
-         Console.WriteLine("Не верное число");
-       }
-      else
-      {
-         Console.WriteLine("Не удалось прочитать число");
-      }
-      Console.Write("Введите номер дня месяца (1-31): ");
-      if (int.TryParse(Console.ReadLine(), out int day) || day < 1 || day > 31)
-      {
-            Console.WriteLine("Не верное число");
-       }
-      else
-      {
-         Console.WriteLine("Не удалось прочитать число");
-      }
-      // Определяем день недели
-      //из начального дня недели и из дня введенего вычитаем единицу потому что начинаем отсчёт с нуля
-      //0 понедельник, 6 - суббота
-      int dayOfWeek = (startDay-1 +day - 1) % 7;
- Console.WriteLine(dayOfWeek);
-      // Проверка на праздник или выходной
-      bool isHoliday = dayOfWeek >= 0 && dayOfWeek < 5;
+    {
+        int startDay;
+        while (true)
+        {
+            Console.Write("Введите номер дня недели, с которого начинается месяц (1-пн, ...7-вс) ");
+            if (int.TryParse(Console.ReadLine(), out startDay) && startDay >= 1 && startDay <= 7)
+            {
+                break;
+            }
+            Console.WriteLine("Неверное число. Пожалуйста, введите число от 1 до 7.");
+        }
 
-      if ( isHoliday)
+        int day;
+        while (true)
+        {
+            Console.Write("Введите день месяца ");
+            if (int.TryParse(Console.ReadLine(), out day) && day >= 1 && day <= 31)
+            {
+                break;
+            }
+            Console.WriteLine("Неверное число. Пожалуйста, введите число от 1 до 31.");
+        }
+
+        Console.WriteLine("---Проверяем выходной ли день ---");
         
+        int dayOfWeek = (startDay - 1 + day - 1) % 7;
+        bool isWeekend = dayOfWeek == 5 || dayOfWeek == 6;
+
+        if (isWeekend)
+        {
+            Console.WriteLine("Выходной день");
+        }
+        else
+        {
          Console.WriteLine("Будний день");
-      else
-          Console.WriteLine("Выходной день");
-   }
+        }
+    }
 }
